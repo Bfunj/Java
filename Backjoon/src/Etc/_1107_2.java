9b53c9c8445b6b2e65293265371ce64bc1223c17
Bfunj <baekhjo9@gmail.com>
2022-09-27 13:53:44 +0900

    2022.09.27

--- Backjoon/src/Etc/_1107_2.java
+++ Backjoon/src/Etc/_1107_2.java
@@ -1,41 +1,45 @@
 package Etc;
 
+/*
+ move_ch : 이동할 채널(String)
+dont_bt.length : 고장난 버튼의 수(String)
+dont_bt[] : 고장난 버튼의 배열(String)
 
+1. 고장난 수랑 같은가?
+
+1.1 같으면 
+비교하는 숫자에 +1, -1 이후 1번 시작
+
+1.2 없으면 숫자 저장
+
+
+ */
 import java.util.Scanner;
 
 public class _1107_2 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int Move_ch = sc.nextInt();
-		int[] DontButton = new int[sc.nextInt()];
+		// input
+		String Move_ch = sc.next();
+		char[] DontButton = new char[sc.nextInt()];
 		for(int i=0;i<DontButton.length;i++) {
-			DontButton[i] = sc.nextInt();
+			DontButton[i] = sc.next().charAt(0);
 			}
-		Boolean Button =false;
-		String result_su="";
-		int Start_ch=100; // 현재 채널
-		String Move_ch_S = Integer.toString(Move_ch); // 이동할 채널을 문자열로 변환
-		char[] Move_ch_C = new char[Move_ch_S.length()]; // 문자열을 문자로 변환
-		for(int i = 0; i<Move_ch_S.length();i++) {
-			Move_ch_C[i]=Move_ch_S.charAt(i);
+		//input 종료
+		
+		String Start_ch="100"; // 현재 채널
+		char[] Move_ch_C = new char[Move_ch.length()]; // 문자열을 문자로 변환
+		for(int i = 0; i<Move_ch.length();i++) {
+			Move_ch_C[i]=Move_ch.charAt(i);
 		}
 		
-		
-		// 첫째 자리에 움직일수 없는 버튼이 있는가?
-		for(int i=0;i<DontButton.length;i++) {
-			if(DontButton[i] == Move_ch_C[0]) Button=true;
-		}
-			if(Button =false) result_su+=Move_ch_C[0];
-			else {
-				
-				
-				
-			}
-			
-		
-
+	
 		sc.close();
+	}
+	
+	public static void comp(String M_ch, int[] donbt) {
+		
 	}
 	
 	
