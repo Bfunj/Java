f570d950af48dfc3023f5c0d98b6e4d2b50b72db
Bfunj <baekhjo9@gmail.com>
2022-11-01 13:52:50 +0900

    2022.11.01

--- Backjoon/src/기하/_1002.java
+++ Backjoon/src/기하/_1002.java
@@ -23,37 +23,64 @@ public class _1002 {
 	
 	static int Axis(int[] axis) {
 		
-		int x1= axis[0];
-		int x2= axis[3];
-		int y1= axis[1];
-		int y2= axis[4];
-		int r1= axis[2];
-		int r2= axis[5];
+		int x2 = 0;
+		int x1 = 0;
+		int y2 = 0;
+		int y1 = 0;
+		int r2 = 0;
+		int r1 = 0;
 		
-		double x1_x2_Reach =Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
-		
+		if(axis[2]>axis[5]) {
+			x2= axis[0];
+			x1= axis[3];
+			y2= axis[1];
+			y1= axis[4];
+			r2= axis[2];
+			r1= axis[5];
+		}
+		if(axis[5]>=axis[2]) {
+			x1= axis[0];
+			x2= axis[3];
+			y1= axis[1];
+			y2= axis[4];
+			r1= axis[2];
+			r2= axis[5];			
+		}
+						
+		double d =Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
+		// 교점이 3개이상인 경우 -- ok
 		if(x1 == x2 && y1 ==y2 && r1==r2) {
 			return -1;
 		}
 		
-		if(x1_x2_Reach > r1+r2) {
+		//교점이 없는 경우
+		//외부
+		if(d > r1+r2) {
 			return 0;
 		}
-		if(x1_x2_Reach == r1+r2) {
+		//내부
+		if(d < r2-r1) {
+			return 0;
+		}
+		if(x1 == x2 && y1 ==y2 && r1 !=r2) {
+			return 0;
+		}
+		
+		// 교점이 하나인 경우
+		//외접
+		if(d == r1+r2) {
 			return 1;
 		}
-		if(r1<r2) {
-			if(x1_x2_Reach + r1 ==r2) {
-				return 1;
-			}
-		}else {
-			if(x1_x2_Reach + r2 ==r1) {
-				return 1;
-			}
+		//내접
+		if(d == r2-r1) {
+			return 1;
 		}
-		if(x1_x2_Reach < r1+r2) {
+		
+		// 교점이 두개인 경우
+		if(d < r1+r2 && d>r2-r1) {
 			return 2;
 		}	
+		
 		return 3;
 	}
 }
---
+++ Backjoon/src/기하/_2477.java
@@ -0,0 +1,25 @@
+package 기하;
+
+import java.util.Scanner;
+
+public class _2477 {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int melon = sc.nextInt();
+		
+		// 1: 동쪽, 2: 서쪽, 3: 남쪽, 4: 북쪽
+		/*
+		 * 
+			1	2	3	4
+			60	160	30	50
+			100		20	
+					
+			
+			
+			100	160	50	20
+			60			30
+		 */
+		
+	}
+}
